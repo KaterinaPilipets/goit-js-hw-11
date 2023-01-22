2a1ca11c14bc6850c55c93dfb4e4efc2ecde96df
KaterinaPilipets <ket.pil.glass@gmail.com>
2023-01-22 10:36:54 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-// import './css/styles.css';
-
 import { Notify } from 'notiflix';
-import { createFetsh } from './fetchCountries';
+import { createFetsh } from './fetch';
+
 const form = document.querySelector('#search-form');
 const input = form.elements.searchQuery;
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
-loadMore.classList.add('visually-hidden');
 
 let page = 0;
 let limit = 0;
 const perPage = 40;
-let totalHits = 0;
+loadMore.classList.add('visually-hidden');
 
 form.addEventListener('submit', onSearch);
 loadMore.addEventListener('click', onLoadMore);
+
 function onLoadMore(event) {
   event.preventDefault();
   page += 1;
@@ -27,41 +26,33 @@ function onSearch(event) {
   event.preventDefault();
   gallery.innerHTML = '';
   loadMore.classList.add('visually-hidden');
-
+  if (!input.value.trim()) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
   createGallery(page);
-  // form.reset();
 }
 
 async function createGallery(page) {
   try {
-    const data = await createFetsh(input.value, page, perPage);
-
-    totalHits = data.totalHits;
-    console.log(totalHits);
-    limit = totalHits / perPage;
-    if (totalHits) {
-      if (page === 1) {
-        Notify.info(`Hooray! We found ${totalHits} images.`);
-      }
-      console.log(page);
-      console.log(limit);
-      if (page > limit) {
-        Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-        return loadMore.classList.add('visually-hidden');
-      }
-
-      renderMurkup(data.hits);
-      loadMore.classList.remove('visually-hidden');
-    } else {
+    const data = await createFetsh(input.value.trim(), page, perPage);
+    limit = data.totalHits / perPage;
+    if (!data.totalHits) {
       loadMore.classList.add('visually-hidden');
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
+    if (page === 1) Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    if (page > limit) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      return loadMore.classList.add('visually-hidden');
+    }
+    renderMurkup(data.hits);
+    loadMore.classList.remove('visually-hidden');
   } catch (error) {
-    console.log(error);
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
@@ -80,19 +71,16 @@ function renderMurkup(arr) {
         comments,
         downloads,
       }) => {
-        return `<div class="photo-card"><a href="${webformatURL}"> 
-        <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        return `<div class="photo-card">
+        <a href="${webformatURL}"><img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
         <div class="info">
-        <p class="info-item"><b>Likes  ${likes}</b></p>
-        <p class="info-item"><b>Views ${views}</b></p>
-        <p class="info-item"><b>Comments ${downloads}</b></p>
-        <p class="info-item"><b>Downloads ${comments}</b></p></div>
+        <p class="info-item"><b>Likes<br>${likes}</b></p>
+        <p class="info-item"><b>Views<br>${views}</b></p>
+        <p class="info-item"><b>Comments<br>${downloads}</b></p>
+        <p class="info-item"><b>Downloads<br>${comments}</b></p></div>
         </div>`;
       }
     )
     .join('');
   gallery.insertAdjacentHTML('beforeend', murkup);
-
-  //   webformatURL - ссылка на маленькое изображение для списка карточек.
-  // largeImageURL - ссылка на большое изображение.
 }
